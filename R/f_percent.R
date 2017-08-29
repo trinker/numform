8535525f7c75303bba6e3adea6d713b8f43cf428
Tyler Rinker <trinker@campuslabs.com>
2017-08-29 10:43:19 -0400

    * `f_prop2percent` and `f_percent` pick up a logical `less_than_replace`   argument that replaces values lower than this with a less than sign and the   cut point specified by the `digits` argument.

--- R/f_percent.R
+++ R/f_percent.R
@@ -7,6 +7,12 @@
 #' @param digits The number of digits to use.  Defaults to 1.  Can be set
 #' globally via: \code{options(numformdigits = n)} where n is the number of
 #' digits beyond the decimal point to include.
+#' @param less_than_replace logical.  If \code{TRUE} values lower than lowest
+#' place value, specified by \code{digits}, will be replaces with a less than
+#' sign followed by the \code{double} representation of the place value
+#' specified by \code{digits}.  For example, if \code{digits = 0} then
+#' replacement will be \code{"<1\%"} or if \code{digits = 2} then replacement will
+#' be \code{"<.01\%"}.
 #' @param \ldots Other values passed to \code{\link[numform]{f_num}}.
 #' @return Returns a string of publication ready digits.
 #' @export
@@ -17,6 +23,10 @@
 #' f_percent(c(30, 33.45, .1), 1)
 #' f_percent(c(0.0, 0, .2, -00.02, 1.122222, pi))
 #' f_prop2percent(c(.30, 1, 1.01, .33, .222, .01))
+#'
+#' f_percent(c(30, 33.45, .1), digits = 0, less_than_replace = TRUE)
+#' f_prop2percent(c(.30, 1, 1.01, .33, .222, .01, .0001, NA), digits = 0,
+#'     less_than_replace = TRUE)
 #'
 #' \dontrun{
 #' if (!require("pacman")) install.packages("pacman")
@@ -31,9 +41,17 @@
 #'         facet_wrap(~cyl, ncol = 1) +
 #'         scale_y_continuous(labels = ff_prop2percent(digits = 0))
 #' }
-f_percent <- function(x, digits = getOption("numformdigits"), ...) {
+f_percent <- function(x, digits = getOption("numformdigits"), less_than_replace = FALSE, ...) {
 
-    f_num(x, digits = digits, s="%", ...)
+    out <- f_num(x, digits = digits, s="%", ...)
+
+    if (isTRUE(less_than_replace)){
+        if (is.null(digits)) digits <- 1
+        repl <- replace_less_than(digits, percent = TRUE)
+        out[x < repl[['prop_cut']]] <- repl[['replacement']]
+    }
+
+    out
 }
 
 #' @export
@@ -49,9 +67,18 @@ ff_percent <- functionize(f_percent)
 #'
 #' @rdname f_percent
 #' @export
-f_prop2percent <- function(x, digits = getOption("numformdigits"), ...) {
+f_prop2percent <- function(x, digits = getOption("numformdigits"), less_than_replace = FALSE, ...) {
 
-    f_num(100*x, digits = digits, s="%", ...)
+    out <- f_num(100*x, digits = digits, s="%", ...)
+
+    if (isTRUE(less_than_replace)){
+        if (is.null(digits)) digits <- 1
+        repl <- replace_less_than(digits, percent = FALSE)
+        out[x < repl[['prop_cut']]] <- repl[['replacement']]
+    }
+
+    out
+
 }
 
 
@@ -60,3 +87,11 @@ f_prop2percent <- function(x, digits = getOption("numformdigits"), ...) {
 #' @rdname f_percent
 ff_prop2percent <- functionize(f_prop2percent)
 
+
+replace_less_than <- function(digits = 0, prefix = "<", percent = FALSE, ...){
+
+    if(percent) div <- 1 else div <- 1e2
+    cut <- 1/(10^digits)
+    list(prop_cut = (cut)/div, replacement = f_percent(cut, digits = digits, prefix = prefix))
+
+}
